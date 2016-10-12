0afb78a6f8ad7d79a850e632acfe3aa0c9745156
monkeyWzr <monkeywzr@gmail.com>
2016-10-12 13:28:13 +0800

    change favicon

--- resources/views/Shared/Partials/GlobalMeta.blade.php
+++ resources/views/Shared/Partials/GlobalMeta.blade.php
@@ -1,7 +1,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{ asset('assets/images/touch/favicon.ico')  }}">
+    <link rel="shortcut icon" href="favicon.ico">
     <link rel="apple-touch-icon" sizes="57x57" href="{{ asset("assets/images/touch/apple-touch-icon-57x57.png") }}">
     <link rel="apple-touch-icon" sizes="114x114" href="{{ asset("assets/images/touch/apple-touch-icon-114x114.png") }}">
     <link rel="apple-touch-icon" sizes="72x72" href="{{ asset("assets/images/touch/apple-touch-icon-72x72.png") }}">
@@ -12,11 +12,11 @@
     <link rel="apple-touch-icon" sizes="152x152" href="{{ asset("assets/images/touch/apple-touch-icon-152x152.png") }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("assets/images/touch/apple-touch-icon-180x180.png") }}">
     <meta name="apple-mobile-web-app-title" content="Attendize">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset("assets/images/touch/favicon-192x192.png") }}">
-    <link rel="icon" type="image/png" sizes="160x160" href="{{ asset("assets/images/touch/favicon-160x160.png") }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset("assets/images/touch/favicon-96x96.png") }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("assets/images/touch/favicon-16x16.png") }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("assets/images/touch/favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="favicon.ico">
+    <link rel="icon" type="image/png" sizes="160x160" href="favicon.ico">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{ url("assets/images/touch/mstile-144x144.png") }}">
     <meta name="msapplication-config" content="{{ url("assets/images/touch/browserconfig.xml") }}">
